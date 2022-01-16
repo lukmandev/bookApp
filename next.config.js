95bd42b7acdc919246ff6c56c154f77439e098ac
lukmandev <spontycsgoonelove@gmail.com>
2022-01-16 15:56:41 +0600

    change api url

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ module.exports = withImages({
         disableStaticImages: true
     },
     env: {
-        // API_URL: 'https://payment-test-app-v2.herokuapp.com',
-        API_URL: 'http://0.0.0.0:8000',
+        API_URL: 'https://payment-test-app-v2.herokuapp.com',
+        // API_URL: 'http://0.0.0.0:8000',
         BASE_URL: 'http://localhost:3000'
     }
 });
